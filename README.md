86e3ac36240802344f5b609a3abb8cc709fd5b1c
梦想 <1076860592@qq.com>
2016-11-21 11:45:07 +0800

    给imageView画圆角

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Navgation
+
+
+# MyNavViewController
 隐藏二级页面的tabbar
 隐藏导航条黑线
+
+
+# BINBaseViewController
+MBProgressHUD
+网络请求
+动画跳转
+
+# UIView+CornerRadio
+给imageView画圆角
---
+++ UIView+CornerRadio.h
@@ -0,0 +1,20 @@
+//
+//  UIView+CornerRadio.h
+//  CornerRadiusTest
+//
+//  Created by 梦想 on 2016/11/21.
+//  Copyright © 2016年 lin_tong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (CornerRadio)
+
+
+/**
+ 绘制圆形icon
+ */
+- (UIImage *)createCornerRadiusWithImage:(UIImage *)image;
+
+
+@end
